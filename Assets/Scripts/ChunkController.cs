7232f6cab6ae28f64a86dab70a72a2a2519cf050
rexthecapt <rexthecapt@outlook.com>
2019-01-23 19:05:55 +0100

    update

--- Assets/Scripts/ChunkController.cs
+++ Assets/Scripts/ChunkController.cs
@@ -1,5 +1,6 @@
 ﻿#region usings
 
+using System;
 using Boo.Lang;
 using UnityEngine;
 using UnityEngine.Experimental.LowLevel;
@@ -11,22 +12,84 @@ public class ChunkController : MonoBehaviour
     public GameObject PlayerGameObject;
     public Transform ChunkParentTransform;
     public Sprite sprite;
-    public bool nextGen = true;
+    public float generationDelayMs = 100f;
+    public bool generateChunkOnce = false;
+    public float chunkMaxAge = 30f;
+
+    private float _generationDelayTicks;
+    private bool _generateChunkOnceTriggerd = false;
+
     void Start()
     {
         GenerateChunk(0, 0);
     }
 
+    void Update()
+    {
+        _generationDelayTicks += Time.deltaTime * 1000;
+
+        if (_generationDelayTicks > generationDelayMs)
+        {
+            _generationDelayTicks = 0;
+            bool chunkGenerated = false;
+
+            PlayerChunkTracker playerChunkTracker = PlayerGameObject.GetComponent<PlayerChunkTracker>();
+            int chunkObjectIndex = playerChunkTracker.CurrentChunkGameObjects.Count - 1;
+            ChunkPosition currentChunkPositionClass = playerChunkTracker.CurrentChunkGameObjects[chunkObjectIndex].GetComponent<ChunkPosition>();
+            //Vector2 currentPositionVector2 = currentChunkPositionClass.Position;
+
+            foreach (GameObject currentChunkGameObject in playerChunkTracker.CurrentChunkGameObjects)
+            {
+                Vector2 currentPositionVector2 = currentChunkGameObject.GetComponent<ChunkPosition>().Position;
+
+                for (int cx = -1; cx <= 1; cx++)
+                {
+                    if (chunkGenerated)
+                        break;
+
+                    for (int cy = -1; cy <= 1; cy++)
+                    {
+                        if (chunkGenerated)
+                            break;
+
+                        Vector2 newPositionVector2 = new Vector2(cx, cy) + currentPositionVector2;
+                        GameObject foundChunkGameObject = GameObject.Find(GetChunkName(newPositionVector2));
+
+                        if (!foundChunkGameObject && !chunkGenerated)
+                        {
+                            GenerateChunk(newPositionVector2);
+                            chunkGenerated = true;
+                            Log("Chunk generated");
+                        }
+                        else
+                        {
+                            foundChunkGameObject.GetComponent<ChunkRemoval>().chunkAge = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void Log(object message)
+    {
+        Debug.Log(string.Format("{0} : {1}", DateTime.UtcNow.TimeOfDay, message));
+    }
+
+    private void GenerateChunk(Vector2 ChunkPosition)
+    {
+        GenerateChunk(ChunkPosition.x, ChunkPosition.y);
+    }
     private void GenerateChunk(float cx, float cy)
     {
-        if(!nextGen)
+        if (!generateChunkOnce)
+            _generateChunkOnceTriggerd = false;
+
+        if (_generateChunkOnceTriggerd)
             return;
 
-        if (genCurr > genLimit)
-            return;
-
-        genCurr++;
-        nextGen = false;
+        if (generateChunkOnce)
+            _generateChunkOnceTriggerd = true;
 
         GameObject Chunk = new GameObject();
         ChunkPosition chunkPosition = Chunk.gameObject.AddComponent<ChunkPosition>();
@@ -48,6 +111,9 @@ public class ChunkController : MonoBehaviour
         Chunk.AddComponent<Rigidbody2D>().gravityScale = 0;
         Chunk.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
 
+        ChunkRemoval chunkRemoval = Chunk.AddComponent<ChunkRemoval>();
+        chunkRemoval.ChunkControllerGameObject = gameObject;
+
         for (int bx = 0 + (int)Chunk.transform.position.x; bx < 16 + (int)Chunk.transform.position.x; bx++)
         {
             for (int by = 0 + (int)Chunk.transform.position.y; by < 16 + (int)Chunk.transform.position.y; by++)
@@ -63,26 +129,12 @@ public class ChunkController : MonoBehaviour
         }
     }
 
-    private int genLimit = 20;
-    private int genCurr = 0;
-
-    void Update()
+    private string GetChunkName(Vector2 ChunkPosition)
     {
-        foreach (GameObject o in PlayerGameObject.GetComponent<PlayerChunkTracker>().CurrentChunkGameObjects)
-        {
-            ChunkPosition currentChunkPosition = o.GetComponent<ChunkPosition>();
-
-            for (int cx = -1; cx <= 1; cx++)
-            {
-                for (int cy = -1; cy <= 1; cy++)
-                {
-                    if (!GameObject.Find("Chunk X" + (currentChunkPosition.Position.x + cx) + " Y" + (currentChunkPosition.Position.y + cy)))
-                    {
-                        GenerateChunk(cx, cy);
-                        Debug.Log("Chunk X" + (currentChunkPosition.Position.x + cx) + " Y" + (currentChunkPosition.Position.y + cy));
-                    }
-                }
-            }
-        }
+        return GetChunkName(ChunkPosition.x, ChunkPosition.y);
+    }
+    private string GetChunkName(float x, float y)
+    {
+        return "Chunk X" + x + " Y" + y;
     }
 }
---
+++ Assets/Scripts/ChunkRemoval.cs
@@ -0,0 +1,40 @@
+﻿#region usings
+
+using UnityEngine;
+
+#endregion
+
+public class ChunkRemoval : MonoBehaviour
+{
+    public GameObject ChunkControllerGameObject;
+    public float chunkAge;
+    public bool playerIsClose;
+
+    private float _maxChunkAge
+    {
+        get { return ChunkControllerGameObject.GetComponent<ChunkController>().chunkMaxAge; }
+    }
+
+    void Update()
+    {
+        if (!playerIsClose)
+            chunkAge += Time.deltaTime;
+        else
+            chunkAge = 0;
+
+        if(chunkAge > _maxChunkAge)
+            Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.gameObject.tag == "Player")
+            playerIsClose = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collider2D)
+    {
+        if (collider2D.gameObject.tag == "Player")
+            playerIsClose = false;
+    }
+}
